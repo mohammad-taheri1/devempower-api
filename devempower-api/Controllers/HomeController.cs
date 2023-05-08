ad8b75578ea028b5470086d8cd204e776806fb49
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-05-08 19:32:27 +0430

    add Home Controller

---
+++ devempower-api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devempower_api.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Home()
+        {
+            return Ok("Welcome To DevEmpower API project");
+        }
+    }
+}
--- devempower-api/Program.cs
+++ devempower-api/Program.cs
@@ -19,12 +19,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(option =>
 {
